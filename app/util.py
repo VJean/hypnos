01234b26bf9faf4630802fed69ba6526824f1177
VJean <jean@vintache.eu>
2016-12-04 00:49:42 +0100

    dump dates as strings

--- app/util.py
+++ app/util.py
@@ -1,10 +1,10 @@
-from datetime import datetime, timedelta
+from datetime import date, datetime, timedelta
 import isodate
 
 
 def dump_datetime(value):
     """Deserialize datetime object into string form for JSON processing."""
-    if isinstance(value, datetime):
+    if isinstance(value, datetime) or isinstance(value, date):
         return value.isoformat()
     elif isinstance(value, timedelta):
         return isodate.duration_isoformat(value)
