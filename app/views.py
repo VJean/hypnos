c32ca74283402b92c6199dd6bc289f2ea70bb035
VJean <jean@vintache.eu>
2017-03-12 16:07:02 +0100

    remove home backgrounds

--- app/views.py
+++ app/views.py
@@ -1,9 +1,7 @@
 from app import app
 from flask import render_template
-from fetch_posts import get_random_redditnugget
 
 
 @app.route('/')
 def homepage():
-    n = get_random_redditnugget()
-    return render_template('index.html', nugget=n)
+    return render_template('index.html')
