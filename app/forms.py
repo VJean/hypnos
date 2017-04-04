7fe433e773abb50e1cbcf61845417c4d0f2fff63
VJean <jean@vintache.eu>
2017-04-04 21:08:19 +0200

    new form with wtforms

---
+++ app/forms.py
@@ -0,0 +1,35 @@
+from datetime import datetime, timedelta
+from flask_wtf import FlaskForm
+from wtforms import DateField, BooleanField, SelectField, DateTimeField
+from wtforms_components import TimeField
+from wtforms.validators import DataRequired, ValidationError
+
+
+class NightForm(FlaskForm):
+    day = DateField('Date', format='%d/%m/%Y', validators=[DataRequired()])
+    to_bed = TimeField('Coucher', format='%H:%M', validators=[DataRequired()])
+    to_rise = TimeField('Lever', format='%H:%M', validators=[DataRequired()])
+    amount = TimeField('Durée', format='%H:%M', validators=[DataRequired()])
+    alone = BooleanField('Seul')
+    sleepless = BooleanField('Nuit blanche')
+    place = SelectField('Lieu', coerce=int)
+
+    def to_bed_datetime(self):
+        date = self.day.data
+        btime = self.to_bed.data
+        rtime = self.to_rise.data
+        if btime > rtime:
+            date = date - timedelta(days=1)
+        return datetime.combine(date, btime)
+
+    def to_rise_datetime(self):
+        date = self.day.data
+        rtime = self.to_rise.data
+        return datetime.combine(date, rtime)
+
+    def amount_timedelta(self):
+        return timedelta(hours=self.amount.data.hour,minutes=self.amount.data.minute)
+
+    def validate_amount(form, field):
+        if timedelta(hours=field.data.hour, minutes=field.data.minute) > (form.to_rise_datetime() - form.to_bed_datetime()):
+            raise ValidationError('La durée de sommeil doit être inférieure à la durée de la nuit.')
--- app/nights.py
+++ app/nights.py
@@ -1,4 +1,8 @@
+from flask import redirect
+from flask import url_for
+from datetime import datetime, timedelta
 from app import app, db
+from app.forms import NightForm
 from app.places import Place
 from flask import render_template, jsonify, request, abort
 from sqlalchemy import func
@@ -18,7 +22,10 @@ class Night(db.Model):
     place_id = db.Column(db.Integer, db.ForeignKey('places.id'))
     place = db.relationship("Place")
 
-    def __init__(self, day, sleepless, begin, end, amount, alone, place):
+    def __init__(self):
+        pass
+
+    def populate(self, day, sleepless, begin, end, amount, alone, place):
         self.day = isodate.parse_date(day)
         self.alone = alone
         self.place = place
@@ -60,9 +67,26 @@ class Night(db.Model):
         }
 
 
-@app.route('/nights', methods=['GET'])
+@app.route('/nights', methods=['GET', 'POST'])
 def show_nights():
-    return render_template('nights.html')
+    form = NightForm()
+    places = []
+    for p in Place.query.all():
+        places.append((p.id, p.name))
+    form.place.choices = places
+    if form.validate_on_submit():
+        new_night = Night()
+        form.populate_obj(new_night)
+
+        new_night.to_bed = form.to_bed_datetime()
+        new_night.to_rise = form.to_rise_datetime()
+        new_night.place = Place.query.get(form.place.data)
+        new_night.amount = form.amount_timedelta()
+
+        db.session.add(new_night)
+        db.session.commit()
+        return redirect(url_for('show_nights'))
+    return render_template('nights2.html', form=form)
 
 
 # API routes
