7f2a593cf5bcff3d631a8c94dab6b1503e87bf63
VJean <jean@vintache.eu>
2017-04-12 22:39:59 +0200

    load custom config if any

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,18 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-
 app = Flask(__name__)
-app.config.from_object('config')
+
+# try to load conf
+basedir = os.path.abspath(os.path.dirname(__file__))
+if os.path.isfile(os.path.join(basedir, "config.py")):
+    print('Loading custom config file.')
+    app.config.from_object('config')
+else:
+    print('No custom config found, loading default file.')
+    app.config.from_object('default_config')
+
+# init models database
 db = SQLAlchemy(app)
 
 
