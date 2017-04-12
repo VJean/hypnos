7c87559f5a26ee798c8fa8b94045336768ff3eab
VJean <jean@vintache.eu>
2017-04-12 23:34:11 +0200

    util: test urls

--- app/util.py
+++ app/util.py
@@ -1,5 +1,7 @@
 from datetime import date, datetime, timedelta
 import isodate
+from urllib.parse import urljoin, urlparse
+from flask import request
 
 
 def dump_datetime(value):
@@ -10,3 +12,9 @@ def dump_datetime(value):
         return isodate.duration_isoformat(value)
 
     raise TypeError("type not serializable")
+
+
+def is_safe_url(target):
+    ref_url = urlparse(request.host_url)
+    test_url = urlparse(urljoin(request.host_url, target))
+    return test_url.scheme in ('http', 'https') and ref_url.netloc == test_url.netloc
