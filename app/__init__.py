34aca94ab67b926a8f7cbbc27f1da165d698cd27
VJean <jean@vintache.eu>
2017-04-12 23:21:35 +0200

    fix loading config

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,15 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 # try to load conf
-basedir = os.path.abspath(os.path.dirname(__file__))
-if os.path.isfile(os.path.join(basedir, "config.py")):
-    print('Loading custom config file.')
+try:
     app.config.from_object('config')
-else:
-    print('No custom config found, loading default file.')
+    print('Loading custom config file.')
+except ImportError:
     app.config.from_object('default_config')
+    print('No custom config found, loading default file.')
 
 # init models database
 db = SQLAlchemy(app)
